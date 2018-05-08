c9265fa1569b717f20542374c4cc0c2b55f7e743
Wesley <wesleymont1@gmail.com>
2018-05-08 02:31:12 -0300

    CountingSort

--- R03-02-Rot-LinearSorting-Countingsort-environment/src/main/java/sorting/linearSorting/CountingSort.java
+++ R03-02-Rot-LinearSorting-Countingsort-environment/src/main/java/sorting/linearSorting/CountingSort.java
@@ -12,8 +12,58 @@ public class CountingSort extends AbstractSorting<Integer> {
 
 	@Override
 	public void sort(Integer[] array, int leftIndex, int rightIndex) {
-		// TODO Auto-generated method stub
-		throw new UnsupportedOperationException("Not implemented yet!");
+		if(leftIndex < rightIndex && array != null) {
+			Integer[] output = copiaArray(array);
+			
+			int maximo = max(array, leftIndex, rightIndex);
+			
+			Integer[] count = new Integer[maximo + 1];
+			
+			// INICIALIZA O COUNT
+			for(int i = 0; i < count.length; i++) {
+				count[i] = 0;
+			}
+			
+			// SOMA +1 NO INDICE QUE É IGUAL AO NUMERO
+			for(int i = leftIndex; i <= rightIndex; i++) {
+				count[array[i]]++;
+			}
+			
+			// SOMA DO NUMERO COM SEU ANTERIOR
+			for(int j = 1; j <= maximo; j++) {
+				count[j] += count[j - 1];
+			}
+			
+			//POSICIONA O INTEIRO NO LOCAL CORRETO
+			for(int k = leftIndex; k <= rightIndex; k++) {
+				int indiceCorreto = --count[array[k]];
+				output[indiceCorreto] = array[k];
+			}
+			
+			
+			for(int l = leftIndex; l <= rightIndex; l++) {
+				array[l] = output[l];
+			}
+			
+		}
+	}
+	
+	private int max(Integer[] array, int leftIndex, int rightIndex) {
+		int maximo = array[leftIndex];
+		for(int i = leftIndex; i <= rightIndex; i++) {
+			if(array[i] > maximo) {
+				maximo = array[i];
+			}
+		}
+		return maximo;
+	}
+	
+	private Integer[] copiaArray(Integer[] array) {
+		Integer[] copia = new Integer[array.length];
+		for(int i = 0; i < array.length; i++) {
+			copia[i] = array[i];
+		}
+		return copia;
 	}
 
 }
--- R03-02-Rot-LinearSorting-Countingsort-environment/src/test/java/sorting/test/StudentSortingTest.java
+++ R03-02-Rot-LinearSorting-Countingsort-environment/src/test/java/sorting/test/StudentSortingTest.java
@@ -7,6 +7,8 @@ import org.junit.Before;
 import org.junit.Test;
 
 import sorting.AbstractSorting;
+import sorting.linearSorting.CountingSort;
+import sorting.linearSorting.ExtendedCountingSort;
 
 public class StudentSortingTest {
 
@@ -36,10 +38,7 @@ public class StudentSortingTest {
 	 * do aluno
 	 */
 	private void getImplementation() {
-		// TODO O aluno deve instanciar sua implementação abaixo ao invés de
-		// null
-		this.implementation = null;
-		Assert.fail("Implementation not provided");
+		this.implementation = new CountingSort();
 	}
 
 	public void populaVetorTamanhoPar(Integer[] arrayPadrao) {
