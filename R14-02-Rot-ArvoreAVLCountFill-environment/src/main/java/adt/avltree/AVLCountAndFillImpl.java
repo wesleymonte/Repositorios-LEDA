9b92b52a41257b85d0faf4948bf58c9a396b347f
wesleymonte <wesley.monte@ccc.ufcg.edu.br>
2018-07-10 16:17:15 -0300

    Alterações Roteiro 14

--- R14-02-Rot-ArvoreAVLCountFill-environment/src/main/java/adt/avltree/AVLCountAndFillImpl.java
+++ R14-02-Rot-ArvoreAVLCountFill-environment/src/main/java/adt/avltree/AVLCountAndFillImpl.java
@@ -1,10 +1,11 @@
 package adt.avltree;
 
+import java.util.Arrays;
+
 import adt.bst.BSTNode;
 import adt.bt.Util;
 
-public class AVLCountAndFillImpl<T extends Comparable<T>> extends
-		AVLTreeImpl<T> implements AVLCountAndFill<T> {
+public class AVLCountAndFillImpl<T extends Comparable<T>> extends AVLTreeImpl<T> implements AVLCountAndFill<T> {
 
 	private int LLcounter;
 	private int LRcounter;
@@ -12,30 +13,28 @@ public class AVLCountAndFillImpl<T extends Comparable<T>> extends
 	private int RLcounter;
 
 	public AVLCountAndFillImpl() {
-		
+
 	}
-	
+
 	@Override
 	protected void rebalance(BSTNode<T> node) {
 		int balancingFactor = calculateBalance(node);
-		if(balancingFactor > 1) {
-			if(calculateBalance((BSTNode<T>) node.getLeft()) >= 0) {
-				Util.rightRotation(node);
+		if (balancingFactor > 1) {
+			if (calculateBalance((BSTNode<T>) node.getLeft()) >= 0) {
 				LLcounter++;
 			} else {
 				Util.leftRotation((BSTNode<T>) node.getLeft());
-				Util.rightRotation(node);
 				LRcounter++;
 			}
-		} else if(balancingFactor < -1) {
-			if(calculateBalance((BSTNode<T>) node.getRight()) <= 0) {
-				Util.leftRotation(node);
+			Util.rightRotation(node);
+		} else if (balancingFactor < -1) {
+			if (calculateBalance((BSTNode<T>) node.getRight()) <= 0) {
 				RRcounter++;
 			} else {
 				Util.rightRotation((BSTNode<T>) node.getRight());
-				Util.leftRotation(node);
 				RLcounter++;
 			}
+			Util.leftRotation(node);
 		}
 	}
 
@@ -61,63 +60,91 @@ public class AVLCountAndFillImpl<T extends Comparable<T>> extends
 
 	@Override
 	public void fillWithoutRebalance(T[] array) {
-		if(isEmpty()) {
+		if (isEmpty()) {
+			T[] xxx = (T[]) new Comparable[array.length];
 			quickSort(array, 0, array.length);
-			insertMiddle(array, 0, array.length);
+			insertMiddle(array, 0, array.length - 1, xxx);
 		} else {
 			T[] oldAVL = order();
 			this.root = new BSTNode<T>();
 			T[] newAVL = concatenateArrays(oldAVL, array);
-			quickSort(newAVL, 0, newAVL.length);
-			insertMiddle(newAVL, 0, newAVL.length);
+			Arrays.sort(newAVL);
+			
+			T[] ordenado = (T[]) new Comparable[array.length];
+			
+			insertMiddle(newAVL, 0, newAVL.length - 1, ordenado);
+
+			int i = 0;
+			int j = (ordenado.length / 2) + 1;
+			int cont = 0;
+			while (j < ordenado.length) {
+				for (int k = 0; k < Math.pow(2, cont); k++) {
+					insert(array[i]);
+					i++;
+				}
+				for (int k = 0; k < Math.pow(2, cont); k++) {
+					insert(array[j]);
+					j++;
+				}
+				cont++;
+			}
 		}
 	}
-	
-	
-	private void insertMiddle(T[] array, int leftIndex, int rightIndex) {
-		if(validateInput(array, leftIndex, rightIndex)) {
-			int middle = (rightIndex - leftIndex) / 2;
-			insert(array[middle]);
-			insertMiddle(array, leftIndex, middle - 1);
-			insertMiddle(array, middle + 1, rightIndex);
+
+	private T[] insertMiddle(T[] array, int leftIndex, int rightIndex, T[] ordenado) {
+		if (validateInput(array, leftIndex, rightIndex)) {
+			int middle = (rightIndex + leftIndex) / 2;
+			insertArray(ordenado, array[middle]);
+			insertMiddle(array, leftIndex, middle - 1, ordenado);
+			insertMiddle(array, middle + 1, rightIndex, ordenado);
+		}
+		return ordenado;
+	}
+
+	private void insertArray(T[] array, T x) {
+		for (int i = 0; i < array.length; i++) {
+			if (array[i] == null) {
+				array[i] = x;
+			}
 		}
 	}
-	
+
 	private void quickSort(T[] array, int leftIndex, int rightIndex) {
-		if(validateInput(array, leftIndex, rightIndex)) {
-			int pi = partition(array ,leftIndex, rightIndex);
+		if (validateInput(array, leftIndex, rightIndex)) {
+			int pi = partition(array, leftIndex, rightIndex);
 			quickSort(array, leftIndex, pi - 1);
 			quickSort(array, pi + 1, rightIndex);
 		}
 	}
-	
+
 	private int partition(T[] array, int leftIndex, int rightIndex) {
 		int pivot = rightIndex;
 		int i = leftIndex - 1;
-		
-		for(int j = leftIndex; j < rightIndex; j++) {
-			if(array[j].compareTo(array[pivot]) <= 0) {
+
+		for (int j = leftIndex; j < rightIndex; j++) {
+			if (array[j].compareTo(array[pivot]) <= 0) {
 				swap(array, ++i, j);
 			}
 		}
 		swap(array, pivot, ++i);
 		return i;
 	}
-	
+
 	private void swap(T[] array, int i, int j) {
 		T temp = array[i];
 		array[i] = array[j];
 		array[j] = array[i];
 	}
-	
+
 	private boolean validateInput(Object[] array, int leftIndex, int rightIndex) {
 		boolean valid = false;
-		if(array != null && array.length > 1 && rightIndex < array.length && leftIndex >= 0 && rightIndex > leftIndex) {
+		if (array != null && array.length > 1 && rightIndex < array.length && leftIndex >= 0
+				&& rightIndex >= leftIndex) {
 			valid = true;
 		}
 		return valid;
 	}
-	
+
 	private T[] concatenateArrays(T[] array1, T[] array2) {
 		T[] rtn = (T[]) new Comparable[array1.length + array2.length];
 		System.arraycopy(array1, 0, rtn, 0, array1.length);
--- R14-02-Rot-ArvoreAVLCountFill-environment/src/main/java/adt/avltree/Test.java
+++ R14-02-Rot-ArvoreAVLCountFill-environment/src/main/java/adt/avltree/Test.java
@@ -7,16 +7,28 @@ public class Test {
 	public static void main(String[] args) {
 		AVLCountAndFill<Integer> tree1 = new AVLCountAndFillImpl<Integer>();
 		AVLCountAndFill<Integer> tree2 = new AVLCountAndFillImpl<Integer>();
-		for (int i = 0; i < 10; i++) {
+		
+		/**
+		for(int i = 0; i < 10; i++){
 			tree1.insert(i);
-			tree2.insert(10 - i);
 		}
+		
 		Integer[] array = new Integer[10];
-		for(int i = 20; i > 10; i--) {
-			array[20 - i] = i;
+		for(int i = 10; i < 20; i++){
+			array[i - 10] = i;
 		}
+		*/
+		
+		tree1.insert(16);
+		Integer[] array = new Integer[16];
+		for(int i = 0; i < 16; i++){
+			array[i] = i;
+		}
+		
 		tree1.fillWithoutRebalance(array);
 		System.out.println(Arrays.toString(tree1.order()));
+		System.out.println(Arrays.toString(tree1.preOrder()));
+		System.out.println(Arrays.toString(tree1.postOrder()));
 	}
 
 }
